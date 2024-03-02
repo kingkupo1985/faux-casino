c270da25f1c3eb4791353964b079dc8e8d945d07
kupo1985 <dave@clientclub.us>
2024-03-02 11:40:14 -0500

    Currenlty works with animation controlled by CSS/JS. The reel image for each reel is created when the page loads, based on the par sheets that are generated for each reel. Odds are 1:200 on par sheet, Reel is then created based on weights in parsheet.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask_scss import Scss
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
-from SlotMachineBrain import SlotMachine
 from PIL import Image
 from datetime import date
 from par_sheet import ParSheet
@@ -31,7 +30,7 @@ Bootstrap(app)
 app.config['SECRET_KEY'] = "MY SECRET KEY"
 
 # Connecting to Database NOT MADE YET FOR USERS
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coin_data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///faux_casino.db'
 # app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy(app)
 
@@ -65,22 +64,26 @@ def slot_machine2():
     for i in range(1, 6):
         par_sheet = ParSheet().generate_par_sheet()
         reels[i] = par_sheet
-
-    return render_template('SlotMachine2.html', images=final_images_urls)
+        print(f"{reels[i]}")
+    final_images_urls, symbol_numbers = create_vertical_images(reels)
+    return render_template('SlotMachine2.html', images=final_images_urls, year=year)
 
 
 # Functions to be moved to class later
-def random_weighted_index(reel):
-    total_weight = sum(symbol_data['weight'] for symbol_data in reel.values())
-    rand_weight = random.uniform(0, total_weight)
-    cumulative_weight = 0
-    for symbol, symbol_data in reel.items():
-        cumulative_weight += symbol_data['weight']
-        if rand_weight < cumulative_weight:
-            return symbol
-    # In case of unexpected situation, return the last symbol
-    return symbol
+# def random_weighted_index(reel):
+#     total_weight = sum(symbol_data['weight'] for symbol_data in reel.values())
+#     rand_weight = random.uniform(0, total_weight)
+#     cumulative_weight = 0
+#     for symbol, symbol_data in reel.items():
+#         cumulative_weight += symbol_data['weight']
+#         if rand_weight < cumulative_weight:
+#             return symbol
+#     # In case of unexpected situation, return the last symbol
+#     return symbol
 
+def random_weighted_index(reel_data):
+    weights = [reel_data[symbol]['weight'] for symbol in reel_data]
+    return random.choices(range(1, len(reel_data) + 1), weights=weights)[0]
 
 def create_vertical_images(reels):
     # Create a list to hold the final images
@@ -93,31 +96,35 @@ def create_vertical_images(reels):
     for i in range(1, 6):
         # Create a blank image with a size large enough to fit all the images
         max_width = 150  # Assuming each image is 150x150
-        total_height = 150 * 48  # Total height for 48 images per reel
+        total_height = 150 * 60  # Total height for 60 images per reel
         final_image = Image.new("RGB", (max_width, total_height))
+        y_offset = 0 # Set Y offset
 
-        y_offset = 0
-        for reel_key, reel_data in reels.items():
-            # Check if the reel belongs to the current group
-            if reel_key == i:
-                for symbol_position in range(1, 49):  # 48 images per reel
+        while y_offset < total_height:
+            for reel_key, reel_data in reels.items():
+                # Check if the reel belongs to the current group
+                if reel_key == i:
+                    # Randomly select a symbol that needs to be added
                     random_index = random_weighted_index(reel_data)
-                    symbol_data = reel_data[random_index]
-
-                    # print(f"line 109: symbol_data: {symbol_data} | reel_data: {reel_data[f'{random_index}']} | random_index = {random_index}")
+                    symbol_data = reel_data[f'symbol_{random_index}']
                     # Get the image URL for the symbol
                     image_url = symbol_data['image_url']
                     file_path = f"static/{image_url}"
                     img = Image.open(file_path)
+
                     # Paste the image onto the final image
                     final_image.paste(img, (0, y_offset))
+
                     # Update the y offset for the next image
                     y_offset += 150
+
                     # Append symbol ID and its position to the list
-                    symbol_positions.append((random_index, symbol_position))
-                    final_image_save_path = f"static/img/final_image_{i}.png"
-                    final_image_path = f"img/final_image_{i}.png"
-                    final_image.save(final_image_save_path)
+                    symbol_positions.append((random_index, y_offset))
+
+        # Create and Append Save Path
+        final_image_save_path = f"static/img/final_image_{i}.png"
+        final_image_path = f"img/final_image_{i}.png"
+        final_image.save(final_image_save_path)
 
         # Append the final image to the list
         final_images_urls.append(final_image_path)
@@ -135,7 +142,7 @@ def create_vertical_images(reels):
 # print(f"{final_image_url}")
 # print(f"{symbol_numbers}")
 
-final_images_urls = ['img/final_image_1.png', 'img/final_image_2.png', 'img/final_image_3.png','img/final_image_4.png', 'img/final_image_5.png']
+# final_images_urls = ['img/final_image_1.png', 'img/final_image_2.png', 'img/final_image_3.png','img/final_image_4.png', 'img/final_image_5.png']
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,3 +1,65 @@
+/**
+ * Setup
+ */
+
+const icon_width = 150,
+      icon_height = 150,
+      num_icons = 60,
+      time_per_icon = 50,
+      indexes = [0,0,0,0,0];
+
+const roll = (reel, offset = 0, finalPosition, duration, startTime) => {
+    // Calculate total distance to move
+    const totalDistance = finalPosition * icon_height;
+
+    // Set initial position
+    const style = getComputedStyle(reel),
+        initialPosition = parseFloat(style.backgroundPositionY);
+
+    // Set transition
+    reel.style.transition = 'none';
+
+    // Start spinning
+    requestAnimationFrame(function spin(timestamp) {
+        const elapsedTime = timestamp - startTime;
+        const progress = Math.min(1, elapsedTime / duration);
+        reel.style.backgroundPositionY = initialPosition + totalDistance * progress + 'px';
+
+        if (progress < 1) {
+            requestAnimationFrame(spin);
+        } else {
+            // Spin completed
+            reel.style.transition = 'none'; // Remove transition
+            reel.style.backgroundPositionY = `${finalPosition * icon_height}px`; // Set final position
+        }
+    });
+};
+
+function rollAll(startTime) {
+    const duration = 4000; // Total duration of the spin (ms)
+
+    const reelsList = document.querySelectorAll('.slots > .reels');
+    [...reelsList].forEach((reel, i) => {
+        // Calculate random final position for each reel
+        const finalPosition = (i + 2) * num_icons + Math.round(Math.random() * num_icons);
+        roll(reel, i, finalPosition, duration, startTime);
+    });
+}
+
+document.getElementById("startButton").addEventListener("click", function() {
+    const startTime = performance.now(); // Define startTime here
+    rollAll(startTime);
+});
+
+// Old scripting below for SlotMachine.html works no repeat image
+function startSlotMachine() {
+    console.log("Start button clicked");
+    // Start sliding animation for each reel
+    //for (let i = 1; i <= 5; i++) {
+    //    slideReel('reel' + i);
+    //}
+}
+
 function startSlotMachine() {
     // Start sliding animation for each reel
     for (let i = 1; i <= 5; i++) {
